d4c9fc8fcc606ac4944d3838f71a4978ed03ae9d
hamzah7800 <hzeb7800pass@gmail.com>
2025-09-13 23:32:41 +0100

    Delete games/crossy-road directory

--- games/crossy-road/metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-
-const config = getDefaultConfig(__dirname);
-
-// Remove all console logs in production...
-config.resolver.assetExts.push("obj", "ttf", "wav", "mp3");
-
-module.exports = config;
